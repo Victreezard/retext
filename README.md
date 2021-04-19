abe2ca855778dcf34803e703bebe9306e0bf5fcc
Victreezard <14161440+Victreezard@users.noreply.github.com>
2021-04-19 17:49:31 +0800

    Applied a more object-oriented approach
    Feature changes:
     - Text will have one font size (along with custom)
     - Text will now be placed at the bottom of the slide
     - Slides marked as points will be Upper Case, others as Title Case

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # retext
 A simple tool to resize text frames of all slides in a PowerPoint presentation
+
+
+Object References:
+- TextRange.ChangeCase - https://docs.microsoft.com/en-us/office/vba/api/powerpoint.ppchangecase
+- Slide.Master - https://docs.microsoft.com/en-us/office/vba/api/powerpoint.slide.master
+- p_align - https://docs.microsoft.com/en-us/office/vba/api/powerpoint.paragraphformat.alignment
+- v_anchor - https://docs.microsoft.com/en-us/office/vba/api/powerpoint.textframe.verticalanchor
+- zorder - https://docs.microsoft.com/en-us/office/vba/api/powerpoint.shape.zorder
--- retext.py
+++ retext.py
@@ -2,66 +2,71 @@ import PySimpleGUI as sg
 import win32com.client
 
 
-ppt = win32com.client.GetActiveObject(
-    "PowerPoint.Application").ActivePresentation
+class Slide_Format():
+    # Defaults: Align Center, Anchor Middle, ZOrder BringToFront
+    def __init__(self, width=0, height=0, left=0, top=0, font_size=50, p_align=2, v_anchor=3,
+                 zorder=2):
+        self.ppt = win32com.client.GetActiveObject(
+            "PowerPoint.Application").ActivePresentation
 
-
-class MySlide():
-    def __init__(self, width, height, left, top, font_size, p_align):
-        self.width = width
-        self.height = height
-        self.left = left
-        self.top = top
+        self.width = self.ppt.Slides(1).Master.Width
+        self.height = int(self.ppt.Slides(1).Master.Height * 0.3)
+        self.left = 0
+        self.top = self.ppt.Slides(1).Master.Height - self.height
         self.font_size = font_size
         self.p_align = p_align
+        self.v_anchor = v_anchor
+        self.zorder = zorder
 
+    def reformat(self, point_list):
+        """For each slide, change the properties of the first object that contains a text."""
+        for slide in self.ppt.Slides:
+            for shape in slide.Shapes:
+                if shape.TextFrame.HasText:
+                    # Slides marked as points will become Upper Case for emphasis else Title Case
+                    if slide.SlideNumber in point_list:
+                        shape.TextFrame.TextRange.ChangeCase(3)
+                    else:
+                        shape.TextFrame.TextRange.ChangeCase(4)
 
-def reshape(point, subtitle, point_list):
-    for count_slide in range(1, ppt.Slides.Count + 1):
-        shape_config = point if str(count_slide) in point_list else subtitle
-        for count_shape in range(1, ppt.Slides(count_slide).Shapes.Count + 1):
-            current_shape = ppt.Slides(count_slide).Shapes(count_shape)
-            if current_shape.TextFrame.HasText:
-                current_shape.Width = shape_config.width
-                current_shape.Height = shape_config.height
-                current_shape.Left = shape_config.left
-                current_shape.Top = shape_config.top
-                current_shape.TextFrame.TextRange.Font.Size = shape_config.font_size
-                current_shape.TextFrame.TextRange.ParagraphFormat.Alignment = shape_config.p_align
-                current_shape.TextFrame.VerticalAnchor = 3
-                current_shape.ZOrder(2)
+                    shape.Width = self.width
+                    shape.Height = self.height
+                    shape.Left = self.left
+                    shape.Top = self.top
+                    shape.TextFrame.TextRange.Font.Size = self.font_size
+                    shape.TextFrame.TextRange.ParagraphFormat.Alignment = self.p_align
+                    shape.TextFrame.VerticalAnchor = self.v_anchor
+                    shape.ZOrder(self.zorder)
 
-
+# Set UI theme and control names
 sg.theme('DarkBlue')
 submit_button = 'GO!'
 exit_button = 'Exit'
 points_input = 'Points'
-point_size = 'PointSize'
-subtitle_size = 'SubtitleSize'
+font_size = 'FontSize'
 
 layout = [
-    [sg.Text('Enter Point Font Size'), sg.InputText(k=point_size)],
-    [sg.Text('Enter Subtitle Font Size'), sg.InputText(k=subtitle_size)],
+    [sg.Text('Enter Title Font Size')],
+    [sg.InputText(k=font_size)],
     [sg.HorizontalSeparator()],
-    [sg.Text('Enter POINT SLIDE NUMBERS separated by spaces'),
-     sg.InputText(k=points_input)],
+    [sg.Text('Enter SLIDE NUMBERS of POINTS separated by spaces')],
+    [sg.InputText(k=points_input)],
     [sg.Button(submit_button), sg.Button(exit_button)]
 ]
 
-my_point = MySlide(700, 800, 10, 0, 95, 1)
-my_subtitle = MySlide(1450, 150, 0, 650, 60, 2)
+slides = Slide_Format()
 window = sg.Window('Reformat Slide Text', layout)
 
+# UI starts here
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == exit_button:
         break
     else:
-        if values[point_size]:
-            my_point.font_size = int(values[point_size])
-        if values[subtitle_size]:
-            my_subtitle.font_size = int(values[subtitle_size])
-        point_list = values[points_input].split()
-        reshape(my_point, my_subtitle, point_list)
+        if values[font_size]:
+            slides.font_size = int(values[font_size])
+        point_list = list(map(int, values[points_input].split()))
+
+        slides.reformat(point_list)
 
 window.close()

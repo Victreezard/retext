a4625d4d017d5494d3d74c9dbd2387e27e763a81
Victreezard <14161440+Victreezard@users.noreply.github.com>
2021-06-23 21:56:39 +0800

    Minor updates:   - Optimized some lines   - Added a progress meter   - Changed to a darker theme

--- retext.py
+++ retext.py
@@ -9,21 +9,21 @@ class Slide_Format():
         self.ppt = win32com.client.GetActiveObject(
             "PowerPoint.Application").ActivePresentation
 
-        self.width = self.ppt.Slides(1).Master.Width
-        self.height = int(self.ppt.Slides(1).Master.Height * 0.3)
-        self.left = 0
-        self.top = self.ppt.Slides(1).Master.Height - self.height
+        self.width = self.ppt.Slides(1).Master.Width if width <= 0 else width
+        self.height = (self.ppt.Slides(1).Master.Height / 3) if height <= 0 else height
+        self.left = left
+        self.top = (self.height * 2) if top <=0 else top
         self.font_size = font_size
         self.p_align = p_align
         self.v_anchor = v_anchor
         self.zorder = zorder
 
     def reformat(self, point_list):
-        """For each slide, change the properties of the first object that contains a text."""
+        """For each slide, reformat shapes that have non-empty text."""
         for slide in self.ppt.Slides:
             for shape in slide.Shapes:
-                if shape.TextFrame.HasText:
-                    # Slides marked as points will become Upper Case for emphasis else Title Case
+                if shape.TextFrame.HasText and shape.TextFrame.TextRange != '':
+                    # Specified Slides will become Upper Case else Title Case
                     if slide.SlideNumber in point_list:
                         shape.TextFrame.TextRange.ChangeCase(3)
                     else:
@@ -37,35 +37,33 @@ class Slide_Format():
                     shape.TextFrame.TextRange.ParagraphFormat.Alignment = self.p_align
                     shape.TextFrame.VerticalAnchor = self.v_anchor
                     shape.ZOrder(self.zorder)
+            sg.one_line_progress_meter('Retexting', slide.SlideNumber, self.ppt.Slides.Count)
 
-# Set UI theme and control names
-sg.theme('DarkBlue')
-submit_button = 'GO!'
-exit_button = 'Exit'
+sg.theme('Black')
+submit_button = 'Retext'
 points_input = 'Points'
 font_size = 'FontSize'
 
 layout = [
-    [sg.Text('Enter Title Font Size')],
+    [sg.Text('Font Size')],
     [sg.InputText(k=font_size)],
-    [sg.HorizontalSeparator()],
-    [sg.Text('Enter SLIDE NUMBERS of POINTS separated by spaces')],
+    [sg.Text('Slides to Capitalize')],
     [sg.InputText(k=points_input)],
-    [sg.Button(submit_button), sg.Button(exit_button)]
+    [sg.Button(submit_button)]
 ]
 
 slides = Slide_Format()
-window = sg.Window('Reformat Slide Text', layout)
+window = sg.Window('Retext', layout)
 
-# UI starts here
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == exit_button:
+    if event == sg.WIN_CLOSED:
         break
     else:
         if values[font_size]:
-            slides.font_size = int(values[font_size])
-        point_list = list(map(int, values[points_input].split()))
+            slides.font_size = int(values[font_size].strip())
+        # Convert each list item from str to int
+        point_list = list(map(int, values[points_input].strip().split()))
 
         slides.reformat(point_list)
 

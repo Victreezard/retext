1eff34ab06e22aff36e500a57f190f5f24d1ead0
Victreezard <14161440+Victreezard@users.noreply.github.com>
2021-07-21 17:26:46 +0800

    - Improved the UI and added font size for titles - Added title icon - Added cfg.py

--- retext.py
+++ retext.py
@@ -1,70 +1,107 @@
-import PySimpleGUI as sg
+import cfg
 import win32com.client
+import PySimpleGUI as sg
 
 
-class Slide_Format():
+class Retext():
     # Defaults: Align Center, Anchor Middle, ZOrder BringToFront
-    def __init__(self, width=0, height=0, left=0, top=0, font_size=50, p_align=2, v_anchor=3,
-                 zorder=2):
+    def __init__(self, width=0, height=0, left=0, top=0, font_size=50, title_font_size=50,
+                 p_align=2, v_anchor=3, zorder=2):
         self.ppt = win32com.client.GetActiveObject(
-            "PowerPoint.Application").ActivePresentation
+            cfg.POWERPOINT).ActivePresentation
 
         self.width = self.ppt.Slides(1).Master.Width if width <= 0 else width
-        self.height = (self.ppt.Slides(1).Master.Height / 3) if height <= 0 else height
+        self.height = (self.ppt.Slides(1).Master.Height /
+                       3) if height <= 0 else height
         self.left = left
-        self.top = (self.height * 2) if top <=0 else top
+        self.top = (self.height * 2) if top <= 0 else top
         self.font_size = font_size
+        self.title_font_size = title_font_size
         self.p_align = p_align
         self.v_anchor = v_anchor
         self.zorder = zorder
 
-    def reformat(self, point_list):
+    @staticmethod
+    def get_number(text):
+        """
+        Return an integer from a text
+        """
+        return int(text.strip())
+
+    @staticmethod
+    def get_numbers(text):
+        """
+        Return a list of integers from a text
+        """
+        return list(map(int, text.strip().split()))
+
+    def reformat(self, title_list):
         """For each slide, reformat shapes that have non-empty text."""
         for slide in self.ppt.Slides:
             for shape in slide.Shapes:
                 if shape.TextFrame.HasText and shape.TextFrame.TextRange != '':
-                    # Specified Slides will become Upper Case else Title Case
-                    if slide.SlideNumber in point_list:
-                        shape.TextFrame.TextRange.ChangeCase(3)
+                    # Slides with Titles will have different properties
+                    if slide.SlideNumber in title_list:
+                        case = cfg.UPPERCASE
+                        font_size = self.title_font_size
                     else:
-                        shape.TextFrame.TextRange.ChangeCase(4)
+                        case = cfg.TITLECASE
+                        font_size = self.font_size
 
-                    shape.Width = self.width
+                    shape.ZOrder(self.zorder)
                     shape.Height = self.height
-                    shape.Left = self.left
                     shape.Top = self.top
-                    shape.TextFrame.TextRange.Font.Size = self.font_size
+                    shape.Left = self.left
+                    shape.Width = self.width
+                    shape.TextFrame.TextRange.ChangeCase(case)
+                    shape.TextFrame.TextRange.Font.Size = font_size
                     shape.TextFrame.TextRange.ParagraphFormat.Alignment = self.p_align
                     shape.TextFrame.VerticalAnchor = self.v_anchor
-                    shape.ZOrder(self.zorder)
-            sg.one_line_progress_meter('Retexting', slide.SlideNumber, self.ppt.Slides.Count)
+
+                    # Unsure why but some slides needs these twice to get proper top value
+                    shape.Top = self.top
+                    shape.Height = self.height
+
+            sg.one_line_progress_meter(
+                'Retexting', slide.SlideNumber, self.ppt.Slides.Count)
+
 
 sg.theme('Black')
-submit_button = 'Retext'
-points_input = 'Points'
-font_size = 'FontSize'
+SUBMIT_BUTTON = 'Retext'
+TITLES_INPUT = 'Slide Numbers'
+FONT_SIZE_INPUT = 'Font Size'
+TITLE_FONT_SIZE_INPUT = 'Title Font Size'
 
 layout = [
-    [sg.Text('Font Size')],
-    [sg.InputText(k=font_size)],
-    [sg.Text('Slides to Capitalize')],
-    [sg.InputText(k=points_input)],
-    [sg.Button(submit_button)]
+    [sg.Fr('Titles', [
+        [sg.Text(TITLES_INPUT)],
+        [sg.InputText(k=TITLES_INPUT)],
+        [sg.Text(FONT_SIZE_INPUT)],
+        [sg.InputText(k=TITLE_FONT_SIZE_INPUT)],
+    ])],
+    [sg.Fr('Others', [
+        [sg.Text(FONT_SIZE_INPUT)],
+        [sg.InputText(k=FONT_SIZE_INPUT)]
+    ])],
+    [sg.Button(SUBMIT_BUTTON)]
 ]
 
-slides = Slide_Format()
-window = sg.Window('Retext', layout)
+rt = Retext()
+window = sg.Window('Reformat slides', layout, icon=cfg.ICON_PATH)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
     else:
-        if values[font_size]:
-            slides.font_size = int(values[font_size].strip())
-        # Convert each list item from str to int
-        point_list = list(map(int, values[points_input].strip().split()))
+        if values[FONT_SIZE_INPUT]:
+            rt.font_size = rt.get_number(values[FONT_SIZE_INPUT])
+        if values[TITLE_FONT_SIZE_INPUT]:
+            rt.title_font_size = rt.get_number(values[TITLE_FONT_SIZE_INPUT])
+        # Parse title slide numbers if input is not empty
+        title_list = rt.get_numbers(
+            values[TITLES_INPUT]) if values[TITLES_INPUT] else []
 
-        slides.reformat(point_list)
+        rt.reformat(title_list)
 
 window.close()
